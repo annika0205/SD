865e391371b74e295d1c2c46c5dd41ea8648a2ef
Annika Meiners <annika.meiners@weidmueller.com>
2025-01-02 17:02:28 +0100

    erstes balkendiagram funktioniert

--- FrontEnd/src/app/sorting/selection-sort/selection-sort.component.ts
+++ FrontEnd/src/app/sorting/selection-sort/selection-sort.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
+import { Chart, registerables } from 'chart.js';
 
+Chart.register(...registerables);
 @Component({
   selector: 'app-selection-sort',
   templateUrl: './selection-sort.component.html',
   styleUrl: './selection-sort.component.css'
 })
-export class SelectionSortComponent {
 
-}
+export class SelectionSortComponent implements AfterViewInit{
+  ngAfterViewInit(): void {
+    
+    var xValues = ["Italy", "France", "Spain", "USA", "Argentina"];
+    var yValues = [55, 49, 44, 24, 15];
+    var barColors = ["red", "green","blue","orange","brown"];
+    
+    const canvas = document.getElementById("myChart") as HTMLCanvasElement;
+
+      const myChart = new Chart("myChart", {
+        type: "bar",
+        data: {
+          labels: xValues,
+          datasets: [
+            {
+              backgroundColor: barColors,
+              data: yValues
+            }
+          ]
+        },
+        options: {
+        }
+      });
+    }
+  }
+
