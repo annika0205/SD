86e0d697afb6e180c45a4107c15dd03b132671ef
Annika Meiners <annika.meiners@weidmueller.com>
2025-03-20 10:48:25 +0100

    gradient-descent verarbeitet funktion und ableitung

--- FrontEnd/src/app/optimizing/gradient-descent/gradient-descent.component.ts
+++ FrontEnd/src/app/optimizing/gradient-descent/gradient-descent.component.ts
@@ -9,10 +9,11 @@ import { ChartService } from '../../sorting/services/chart.service';
   styleUrls: ['./gradient-descent.component.css']
 })
 export class GradientDescentComponent implements AfterViewInit {
-  exponents = [4, 3, 2, 1, 0];
   predefinedTexts = ['x^4', 'x^3', 'x^2', 'x', ''];
-  inputArray = Array(5).fill('');  // Initialize with 5 empty inputs
-  inputs: string[] = ["-3", "0.8", "30"];
+  parameters: string[] = ["-3", "0.8", "30"];
+  inputs: string[] = ["", "", "", "", ""];
+  function: number[] = [0, 0, 1, 0, 0]; //x^2 als startfunktion
+  differential: number[] = [0, 0, 2, 0]; //2x als Startableitung
 
   constructor(
     private gradientDescentService: GradientDescentService,
@@ -30,13 +31,18 @@ export class GradientDescentComponent implements AfterViewInit {
   }
 
   onClick() {
-    const startX = parseFloat(this.inputs[0]);
-    const alpha = parseFloat(this.inputs[1]);
-    const steps = parseInt(this.inputs[2], 10);
+    const startX = parseFloat(this.parameters[0]);
+    const alpha = parseFloat(this.parameters[1]);
+    const steps = parseInt(this.parameters[2], 10);
     this.gradientDescentService.gradientDescent(startX, alpha, steps, this.chartService);
   }
 
-  gradient(x: number): number {
-    return 2 * x;
+  gradient(x: number): number[] {
+    console.log(this.inputs);
+    this.function = this.inputs.map(num => parseInt(num.trim(), 10)).filter(num => !isNaN(num));
+    for (let i = this.differential.length-1; i >0; i--){
+      this.differential[i] = this.function[i+1] * this.differential.length-i;
+    }
+    return this.differential;
   }
 }
