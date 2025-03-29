96006828e4fc7f5f906e6269b3746be92c87a95d
Annika Meiners <annika.meiners@weidmueller.com>
2025-03-29 14:29:10 +0100

    routing zu startseiten klappt wieder

--- FrontEnd/src/app/optimizing/optimizing-routing.module.ts
+++ FrontEnd/src/app/optimizing/optimizing-routing.module.ts
@@ -1,7 +1,9 @@
 import { Routes, RouterModule } from "@angular/router";
 import { GradientDescentComponent } from "./gradient-descent/gradient-descent.component";
+import { OptimizingComponent } from "./optimizing.component";
 
 export const Optimisation_routes: Routes = [
  // {path:'', component: SortingStartComponent},
   {path:'gradientdescent', component: GradientDescentComponent},
+  {path:'', component: OptimizingComponent},
 ];
